7ca5837edbb7a909e6092722cfce2fabf83d2fa5
Francisco Daniel Cornejo Mora <fdcornejo@scio.local>
2019-09-05 17:29:53 -0500

    Primer commit y ultimo

--- Composite/Program.cs
+++ Composite/Program.cs
@@ -14,15 +14,20 @@ namespace Composite
             root.Add(new PrimitiveElement("Red Line"));
             root.Add(new PrimitiveElement("Blue Circle"));
             root.Add(new PrimitiveElement("Green Box"));
+            root.Add(new PrimitiveElement("Soy el Adoptado"));
 
             // Create a branch
 
-            CompositeElement comp =
-              new CompositeElement("Two Circles");
+            CompositeElement comp = new CompositeElement("Two Circles");
+            CompositeElement otro = new CompositeElement("Hermano Feo");
+
             comp.Add(new PrimitiveElement("Black Circle"));
             comp.Add(new PrimitiveElement("White Circle"));
+            
+            comp.Add(otro);
+            otro.Add(new PrimitiveElement("El hijo Feo"));
+            otro.Add(new PrimitiveElement("El hijo Mas Feo"));
             root.Add(comp);
-
             // Add and remove a PrimitiveElement
 
             PrimitiveElement pe =
